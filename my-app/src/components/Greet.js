61f3ed9ba904ef330056603fa5a8642086c3fdd8
ankit.varia <ankit.varia@deskera.org>
2022-05-27 07:21:20 +0530

    total print

--- my-app/src/components/Greet.js
+++ my-app/src/components/Greet.js
@@ -4,19 +4,19 @@ import World from './world'
 const Greet = (props) => {
   console.log("Props -->", props)
 
-  // let sum = 0;
-  // const total = () => {
-  //   props.values.forEach((value) => {
-  //     sum += value
-  //   });
+  let sum = 0;
+  const total = () => {
+    props.values.forEach((value) => {
+      sum += value
+    });
 
-  //   return <h1>{sum}</h1>;
-  // }
+    return <h1>{sum}</h1>;
+  }
 
   return (
     <div>
       {props.children}
-      <h1>Greeting from :: {props.name} ::: & designation  is :: {props.designation}</h1>
+      <h1>Greeting from :: {props.name} ::: & designation  is :: {props.designation} &&  {total()}</h1>
       <World name={props.name} xyz="testtest" />
     </div>
 
