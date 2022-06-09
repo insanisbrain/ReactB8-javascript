6a6b5b295992d0d62284a50246b15e1e8b9fb05e
ankit.varia <ankit.varia@deskera.org>
2022-06-09 08:21:13 +0530

    DataFetching

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Home from './components/RoutingComponents/Home';
-import About from './components/RoutingComponents/About';
-// Routing
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/RoutingComponents/Navbar';
-import Order from './components/RoutingComponents/Order';
-import ComponentC from './components/useContext/ComponentC';
 import React from 'react';
-import CounterOne from './components/useReducer/CounterOne';
-import CounterTwo from './components/useReducer/CounterTwo';
-
-export const UserContext = React.createContext();
-export const ChannelContext = React.createContext();
-
-let bigObject = {
-  name: "ankit",
-  age: 27,
-  designation: []
-}
+import DataFetching from './components/useReducer/DataFetching';
 
 function App() {
   return (
     <div className="App">
-      {/* <CounterOne /> */}
-      <CounterTwo />
+      <DataFetching />
     </div>
   );
 }
