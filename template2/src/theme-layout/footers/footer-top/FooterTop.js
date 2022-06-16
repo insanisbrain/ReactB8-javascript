12af778e8ebbc8680cbb4784150445a08f544468
ankit.varia <ankit.varia@deskera.org>
2022-06-16 08:46:41 +0530

    Template 2 Project

---
+++ template2/src/theme-layout/footers/footer-top/FooterTop.js
@@ -0,0 +1,97 @@
+import React from 'react'
+
+const FooterTop = () => {
+  return (
+    <>
+      <div className="footer-top-wrap gray-bg">
+        <div className="container">
+          <div className="container-inner-wrap">
+            <div className="row">
+              <div className="col">
+                <div className="footer-widget">
+                  <div className="fw-title">
+                    <h5 className="title">Product</h5>
+                  </div>
+                  <div className="fw-link">
+                    <ul>
+                      <li><a href="services.html">Features</a></li>
+                      <li><a>Pricing</a></li>
+                      <li><a>Create a free account</a></li>
+                      <li><a>Privacy Policy & GDPR</a></li>
+                      <li><a>Terms of Service</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="footer-widget">
+                  <div className="fw-title">
+                    <h5 className="title">Company</h5>
+                  </div>
+                  <div className="fw-link">
+                    <ul>
+                      <li><a href="about-us.html">About Us</a></li>
+                      <li><a>FunnelJoy</a></li>
+                      <li><a href="blog.html">Blog</a></li>
+                      <li><a href="contact.html">Contact Us</a></li>
+                      <li><a>Privacy Policy</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="footer-widget">
+                  <div className="fw-title">
+                    <h5 className="title">SUPPORT & TRAINING</h5>
+                  </div>
+                  <div className="fw-link">
+                    <ul>
+                      <li><a href="contact.html">Support</a></li>
+                      <li><a href="index.html">Knowledge Base</a></li>
+                      <li><a>Webinars</a></li>
+                      <li><a>API Documentation</a></li>
+                      <li><a>Log In</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="footer-widget">
+                  <div className="fw-title">
+                    <h5 className="title">Resources</h5>
+                  </div>
+                  <div className="fw-link">
+                    <ul>
+                      <li><a>API reference</a></li>
+                      <li><a>Status</a></li>
+                      <li><a href="contact.html">Get help</a></li>
+                      <li><a>Brand assets</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="footer-widget text-left text-xl-right">
+                  <div className="fw-title">
+                    <h5 className="title">SOCIAL MEDIA CONNECT</h5>
+                  </div>
+                  <div className="fw-social">
+                    <ul>
+                      <li><a><i className="fab fa-facebook-f"></i></a></li>
+                      <li><a><i className="fab fa-twitter"></i></a></li>
+                      <li><a><i className="fab fa-instagram"></i></a></li>
+                      <li><a><i className="fab fa-linkedin-in"></i></a></li>
+                      <li><a><i className="fab fa-youtube"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default FooterTop
